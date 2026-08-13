6965bd2aaa5c0920cca164fb4b9ac3ffa0a9db2b
iagozlight <iago.chagas@escola.pr.gov.br>
2026-08-12 21:00:04 -0300

    Começando o projeto, adicionei a pasta do spring initializr dentro do repositório, adicionei a pasta do  Controller, os Entity, os repository e os service.

---
+++ Logidutra/src/main/java/br/com/uniamerica/Logidutra/LogidutraApplication.java
@@ -0,0 +1,13 @@
+package main.java.br.com.uniamerica.Logidutra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogidutraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogidutraApplication.class, args);
+	}
+
+}
---
+++ Logidutra/src/test/java/br/com/uniamerica/Logidutra/LogidutraApplicationTests.java
@@ -0,0 +1,13 @@
+package test.java.br.com.uniamerica.Logidutra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogidutraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

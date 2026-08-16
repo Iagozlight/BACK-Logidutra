98df92bd2415280cea1393dd918045d8fd7d0153
PauloBaier <baierpaulo312@gmail.com>
2026-08-16 17:04:30 -0300

    Controller de Veiculos completa

--- Logidutra/src/main/java/br/com/uniamerica/Logidutra/service/VeiculoService.java
+++ Logidutra/src/main/java/br/com/uniamerica/Logidutra/service/VeiculoService.java
@@ -1,6 +1,5 @@
 package br.com.uniamerica.Logidutra.service;
 
-import java.security.InvalidParameterException;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -14,7 +13,7 @@ import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
-public class VeiculosService {
+public class VeiculoService {
     private VeiculoRepository veiculoRepository;
 
     public boolean validarPlaca(String placa){
@@ -43,7 +42,7 @@ public class VeiculosService {
         return veiculo;
     }
 
-    public VeiculoEntity buscarPorId(long id){
+    public VeiculoEntity buscarPorId(Long id){
         VeiculoEntity veiculo = veiculoRepository.findById(id).orElseThrow();
         return veiculo;
     }
@@ -52,27 +51,31 @@ public class VeiculosService {
         return veiculoRepository.findAll();
     }
 
-    public VeiculoEntity atualizar(long id, VeiculoRequest veiculoRequest){
+    public VeiculoEntity atualizar(Long id, VeiculoRequest veiculoRequest){
         VeiculoEntity veiculo = this.buscarPorId(id);
 
         veiculo.setMarca(veiculoRequest.marca());
         veiculo.setModelo(veiculoRequest.modelo());
         veiculo.setPlaca(veiculoRequest.placa());
 
+        veiculo = veiculoRepository.save(veiculo);
+
         return veiculo;
     }
 
-    public VeiculoEntity atualizarParcial(long id, VeiculoRequest veiculoRequest){
+    public VeiculoEntity atualizarParcial(Long id, VeiculoRequest veiculoRequest){
         VeiculoEntity veiculo = this.buscarPorId(id);
 
         if(veiculoRequest.marca() != null) veiculo.setMarca(veiculoRequest.marca());
         if(veiculoRequest.modelo() != null) veiculo.setModelo(veiculoRequest.modelo());
         if(veiculoRequest.placa() != null) veiculo.setPlaca(veiculoRequest.placa());
 
+        veiculo = veiculoRepository.save(veiculo);
+
         return veiculo;
     }
 
-    public void deletar(long id){
+    public void deletar(Long id){
         VeiculoEntity veiculo = this.buscarPorId(id);
         veiculoRepository.delete(veiculo);
     }

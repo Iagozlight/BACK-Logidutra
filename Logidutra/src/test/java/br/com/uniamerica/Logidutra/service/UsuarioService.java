e6ff0b186143e131ae4f4292814122a2e0533c9b
iagozlight <iago.chagas@escola.pr.gov.br>
2026-08-16 21:14:54 -0300

    Finalizado todos os CRUDS e atualizar, listar, deletar, atualizar parcial e salvar, tanto no Controller quanto no Service

--- Logidutra/src/test/java/br/com/uniamerica/Logidutra/service/UsuarioService.java
+++ Logidutra/src/test/java/br/com/uniamerica/Logidutra/service/UsuarioService.java
@@ -4,7 +4,11 @@ import br.com.uniamerica.Logidutra.controller.dto.UsuarioRequest;
 import br.com.uniamerica.Logidutra.entity.Usuario;
 import br.com.uniamerica.Logidutra.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -32,9 +36,49 @@ public class UsuarioService {
         if (!usuario.getSenha().equals(senha)) {
             throw new RuntimeException("Senha incorreta");
         }
-
         return usuario;
     }
+
+    public List<Usuario> listar(){
+        return this.usuarioRepository.findAll();
+    }
+
+    public Usuario buscarPorId(long id){
+        return this.usuarioRepository.findById(id)
+                .orElseThrow(
+                        () -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                                "Usuario " + id + " Não encontrado")
+                );
+    }
+
+    public Usuario atualizar(long id, UsuarioRequest usuarioRequest) {
+        Usuario usuario1 = this.buscarPorId(id);
+
+        usuario1.setNome(usuarioRequest.nome());
+        usuario1.setSenha(usuarioRequest.senha());
+        usuario1.setIdade(usuarioRequest.idade());
+        usuario1.setPerm(usuarioRequest.perm());
+
+        return this.usuarioRepository.save(usuario1);
+    }
+
+    public Usuario atualizarParcial(long id, UsuarioRequest usuarioRequest) {
+        Usuario usuario = this.buscarPorId(id);
+
+        if(usuarioRequest.nome() != null) usuario.setNome(usuarioRequest.nome());
+        if(usuarioRequest.senha() != null) usuario.setSenha(usuarioRequest.senha());
+        if(usuarioRequest.idade() != null) usuario.setIdade(usuarioRequest.idade());
+        if(usuarioRequest.perm() != null) usuario.setPerm(usuarioRequest.perm());
+
+        return this.usuarioRepository.save(usuario);
+
+    }
+
+    public void deletarPorId(long id) {
+        this.buscarPorId(id);
+        this.usuarioRepository.deleteById(id);
+    }
+
 
 
 }

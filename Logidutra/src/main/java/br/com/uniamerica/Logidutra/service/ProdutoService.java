1514f9cc6619401bfe1c1cdceac4ce6cd4191890
Mateussantos1207 <mateussntos99@gmail.com>
2026-09-15 20:17:58 -0300

    feat: crud completo Romaneios

--- Logidutra/src/main/java/br/com/uniamerica/Logidutra/service/ProdutoService.java
+++ Logidutra/src/main/java/br/com/uniamerica/Logidutra/service/ProdutoService.java
@@ -6,17 +6,21 @@ import br.com.uniamerica.Logidutra.entity.Produto;
 import br.com.uniamerica.Logidutra.entity.Usuario;
 import br.com.uniamerica.Logidutra.repository.ProdutoRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ProdutoService {
 
     private final ProdutoRepository produtoRepository;
+    private Logger logger;
 
     public Produto salvar (ProdutoRequest produtoRequest){
         Produto produto1 = new Produto();
--- Logidutra/src/main/java/br/com/uniamerica/Logidutra/service/RomaneiosService.java
+++ Logidutra/src/main/java/br/com/uniamerica/Logidutra/service/RomaneiosService.java
@@ -1,13 +1,19 @@
 package br.com.uniamerica.Logidutra.service;
 
+import br.com.uniamerica.Logidutra.controller.dto.ProdutoRequest;
 import br.com.uniamerica.Logidutra.controller.dto.RomaneiosRequest;
+import br.com.uniamerica.Logidutra.controller.dto.RomaneiosResponse;
+import br.com.uniamerica.Logidutra.entity.Produto;
 import br.com.uniamerica.Logidutra.entity.Romaneios;
 import br.com.uniamerica.Logidutra.repository.RomaneiosRepository;
 import jakarta.persistence.Entity;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 @Service
@@ -28,14 +34,38 @@ public class RomaneiosService {
         romaneiosnovo.setData(romaneiosRequest.data());
 
         log.info("romaneio aprovado e salvo");
-
-        try {
-            log.error("Teste");
-        }catch (Exception e){
-
-        }
-
         return this.romaneiosRepository.save(romaneiosnovo);
+    }
 
+    public List <Romaneios> listar (){
+        return this.romaneiosRepository.findAll();
+    }
+
+    public Romaneios buscarPorId (long id){
+        return this.romaneiosRepository.findById(id)
+                .orElseThrow(
+                        () -> new ResponseStatusException(HttpStatus.NOT_FOUND)
+                );
+    }
+
+    public Romaneios atualizar (long id, RomaneiosRequest romaneiosRequest) {
+        Romaneios romaneios = this.buscarPorId(id);
+
+        romaneios.setData(romaneiosRequest.data());
+
+        return this.romaneiosRepository.save(romaneios);
+    }
+
+    public Romaneios atualizarParcial (long id, RomaneiosRequest romaneiosRequest){
+        Romaneios romaneios = this.buscarPorId(id);
+
+        if(romaneiosRequest.data()!= null) romaneios.setData(romaneiosRequest.data());
+
+        return this.romaneiosRepository.save(romaneios);
+    }
+
+    public void deletarPorID(long id){
+        this.buscarPorId(id);
+        this.romaneiosRepository.deleteById(id);
     }
 }

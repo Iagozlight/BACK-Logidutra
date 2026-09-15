689f6eb0e8e52e6d5e1545a0c6c3e6e53f098c53
Thales <thalesantos12@gmail.com>
2026-09-15 01:30:35 -0300

    chore: adiciona @EnableFeignClients na Application que roda o servidor, para que possa ser utilizado ViaCep

--- Logidutra/src/main/java/br/com/uniamerica/Logidutra/LogidutraApplication.java
+++ Logidutra/src/main/java/br/com/uniamerica/Logidutra/LogidutraApplication.java
@@ -3,8 +3,10 @@ package br.com.uniamerica.Logidutra;
 import org.aspectj.weaver.ast.Test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class  LogidutraApplication {
 
 	public static void main(String[] args) {

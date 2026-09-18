a856741df00639e257646c326ec45107d15c6d7d
Thales <thalesantos12@gmail.com>
2026-09-18 00:52:40 -0300

    feat: cria metodo consultarCep, verificando se existe ou nao

--- Logidutra/src/main/java/br/com/uniamerica/Logidutra/service/ClienteService.java
+++ Logidutra/src/main/java/br/com/uniamerica/Logidutra/service/ClienteService.java
@@ -2,6 +2,8 @@ package br.com.uniamerica.Logidutra.service;
 
 import br.com.uniamerica.Logidutra.controller.dto.ClienteRequest;
 import br.com.uniamerica.Logidutra.controller.dto.ClienteResponse;
+import br.com.uniamerica.Logidutra.controller.dto.ViaCepResponse;
+import br.com.uniamerica.Logidutra.data.feign.ViaCepClient;
 import br.com.uniamerica.Logidutra.entity.ClienteEntity;
 import br.com.uniamerica.Logidutra.entity.Usuario;
 import br.com.uniamerica.Logidutra.entity.VeiculoEntity;
@@ -9,19 +11,42 @@ import br.com.uniamerica.Logidutra.repository.ClienteRepository;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class ClienteService {
+
+    private final Logger logger;
 
     @Autowired
     private ClienteRepository clienteRepository;
+
+    private final ViaCepClient viaCepClient;
+
+    private ViaCepResponse consultarCep(String cep) {
+        log.info("Consultando CEP {} na ViaCEP", cep);
+        ViaCepResponse viaCepResponse = viaCepClient.buscarPorCep(cep);
+
+        if (viaCepResponse == null || Boolean.TRUE.equals(viaCepResponse.erro())) {
+            log.warn("CEP {} não encontrado na ViaCEP", cep);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "CEP não encontrado: " + cep);
+        }
+
+        return viaCepResponse;
+    }
 
     @Transactional
     public ClienteEntity salvar(ClienteRequest clienteRequest) {
@@ -35,7 +60,7 @@ public class ClienteService {
         return clienteRepository.save(clienteEntity);
     }
 
-    public List<ClienteEntity> listar(){
+    public List<ClienteEntity> listar() {
         return this.clienteRepository.findAll();
     }
 
@@ -45,16 +70,13 @@ public class ClienteService {
     }
 
     @Transactional
-    public ClienteEntity atualizar(Long id, ClienteRequest clienteRequest){
+    public ClienteEntity atualizar(Long id, ClienteRequest clienteRequest) {
         ClienteEntity clienteEntity = this.buscarPorId(id);
 
         clienteEntity.setNome(clienteRequest.nome());
         clienteEntity.setCpf(clienteRequest.cpf());
         clienteEntity.setTelefone(clienteRequest.telefone());
         clienteEntity.setCep(clienteRequest.cep());
-        clienteEntity.setLogradouro(clienteRequest.logradouro());
-        clienteEntity.setBairro(clienteRequest.bairro());
-        clienteEntity.setCidade(clienteRequest.cidade());
 
         clienteEntity = clienteRepository.save(clienteEntity);
         return clienteEntity;
@@ -64,13 +86,10 @@ public class ClienteService {
     public ClienteEntity atualizarParcial(Long id, ClienteRequest clienteRequest) {
         ClienteEntity clienteEntity = this.buscarPorId(id);
 
-        if(clienteRequest.nome() != null) clienteEntity.setNome(clienteRequest.nome());
-        if(clienteRequest.cpf() != null) clienteEntity.setCpf(clienteRequest.cpf());
-        if(clienteRequest.telefone() != null) clienteEntity.setTelefone(clienteRequest.telefone());
-        if(clienteRequest.cep() != null) clienteEntity.setCep(clienteRequest.cep());
-        if(clienteRequest.logradouro() != null) clienteEntity.setLogradouro(clienteRequest.logradouro());
-        if(clienteRequest.bairro() != null) clienteEntity.setBairro(clienteRequest.bairro());
-        if(clienteRequest.cidade() != null) clienteEntity.setCidade(clienteRequest.cidade());
+        if (clienteRequest.nome() != null) clienteEntity.setNome(clienteRequest.nome());
+        if (clienteRequest.cpf() != null) clienteEntity.setCpf(clienteRequest.cpf());
+        if (clienteRequest.telefone() != null) clienteEntity.setTelefone(clienteRequest.telefone());
+        if (clienteRequest.cep() != null) clienteEntity.setCep(clienteRequest.cep());
 
         clienteEntity = clienteRepository.save(clienteEntity);
         return clienteEntity;

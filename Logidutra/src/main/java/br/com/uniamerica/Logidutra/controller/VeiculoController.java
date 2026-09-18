d62587da20ce918c171bf1eac179e79a3a64993c
Thales <thalesantos12@gmail.com>
2026-09-18 02:04:56 -0300

    feat: adiciona dois verbos Patch, com os metodos para que possa marcar em rota ou disponivel para o veiculo

--- Logidutra/src/main/java/br/com/uniamerica/Logidutra/controller/VeiculoController.java
+++ Logidutra/src/main/java/br/com/uniamerica/Logidutra/controller/VeiculoController.java
@@ -23,6 +23,7 @@ import br.com.uniamerica.Logidutra.controller.dto.VeiculoResponse;
 import br.com.uniamerica.Logidutra.entity.VeiculoEntity;
 import br.com.uniamerica.Logidutra.service.VeiculoService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/logidutra/veiculo")
@@ -32,85 +33,99 @@ public class VeiculoController {
     private final VeiculoService veiculoService;
 
     @PostMapping
-    public ResponseEntity<VeiculoResponse> salvar(@RequestBody @Valid VeiculoRequest veiculoRequest){
-        
-        try{
+    public ResponseEntity<VeiculoResponse> salvar(@RequestBody @Valid VeiculoRequest veiculoRequest) {
+
+        try {
             VeiculoEntity veiculo = veiculoService.salvar(veiculoRequest);
             return new ResponseEntity<VeiculoResponse>(VeiculoResponse.de(veiculo), HttpStatus.CREATED);
 
-        }catch(IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }catch(Exception e){
+        } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.BAD_GATEWAY);
         }
     }
 
     @GetMapping
-    public ResponseEntity<List<VeiculoResponse>> listar(){
-        try{
+    public ResponseEntity<List<VeiculoResponse>> listar() {
+        try {
             List<VeiculoResponse> lista = veiculoService.listar().stream().map(item -> VeiculoResponse.de(item)).collect(Collectors.toList());
             return new ResponseEntity<List<VeiculoResponse>>(lista, HttpStatus.OK);
-        }catch(Exception e){
+        } catch (Exception e) {
             return new ResponseEntity<List<VeiculoResponse>>(HttpStatus.BAD_GATEWAY);
         }
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<VeiculoResponse> buscarPorId(@PathVariable Long id){
-        try{
+    public ResponseEntity<VeiculoResponse> buscarPorId(@PathVariable Long id) {
+        try {
             VeiculoEntity veiculo = veiculoService.buscarPorId(id);
             return new ResponseEntity<VeiculoResponse>(VeiculoResponse.de(veiculo), HttpStatus.OK);
-        }
-        catch(NoSuchElementException e){
+        } catch (NoSuchElementException e) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.BAD_GATEWAY);
         }
     }
 
     @PutMapping
-    public ResponseEntity<VeiculoResponse> atualizarVeiculo(@RequestParam(required=true) Long id, @RequestBody @Valid VeiculoRequest veiculoRequest){
-        try{
+    public ResponseEntity<VeiculoResponse> atualizarVeiculo(@RequestParam(required = true) Long id, @RequestBody @Valid VeiculoRequest veiculoRequest) {
+        try {
             VeiculoEntity veiculo = veiculoService.atualizar(id, veiculoRequest);
             return new ResponseEntity<>(VeiculoResponse.de(veiculo), HttpStatus.OK);
-        }catch(IllegalArgumentException e){
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-    }
-        catch(NoSuchElementException e){
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (NoSuchElementException e) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.BAD_GATEWAY);
         }
     }
 
     @PatchMapping
-    public ResponseEntity<VeiculoResponse> atualizarParcialVeiculo(@RequestParam(required=true) Long id, @RequestBody @Valid VeiculoRequest veiculoRequest){
-        try{
+    public ResponseEntity<VeiculoResponse> atualizarParcialVeiculo(@RequestParam(required = true) Long id, @RequestBody @Valid VeiculoRequest veiculoRequest) {
+        try {
             VeiculoEntity veiculo = veiculoService.atualizarParcial(id, veiculoRequest);
             return new ResponseEntity<>(VeiculoResponse.de(veiculo), HttpStatus.OK);
-        }catch(IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-        catch(NoSuchElementException e){
+        } catch (NoSuchElementException e) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.BAD_GATEWAY);
         }
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<VeiculoResponse> deletar(@PathVariable Long id){
-        try{
-            veiculoService.deletar(id);
-            return new ResponseEntity<>(HttpStatus.OK);
+    @PatchMapping("/{id}/em-rota")
+    public ResponseEntity<VeiculoResponse> marcarEmRota(@PathVariable Long id) {
+        try {
+            VeiculoEntity veiculo = veiculoService.marcarEmRota(id);
+            return new ResponseEntity<>(VeiculoResponse.de(veiculo), HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (ResponseStatusException e) {
+            return new ResponseEntity<>(e.getStatusCode());
         }
-        catch(NoSuchElementException e){
+    }
+
+    @PatchMapping("/{id}/disponivel")
+    public ResponseEntity<VeiculoResponse> marcarDisponivel(@PathVariable Long id) {
+        try {
+            VeiculoEntity veiculo = veiculoService.marcarDisponivel(id);
+            return new ResponseEntity<>(VeiculoResponse.de(veiculo), HttpStatus.OK);
+        } catch (NoSuchElementException e) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        catch(Exception e){
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<VeiculoResponse> deletar(@PathVariable Long id) {
+        try {
+            veiculoService.deletar(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.BAD_GATEWAY);
         }
     }
